5449d1727aebf24e4ee1b5f97d359534163c214b
BhargaviD2 <166976522+BhargaviD2@users.noreply.github.com>
2026-02-08 14:36:35 +0530

    Create Index.tsx for home page of SmileCare dental clinic website

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,41 +1,26 @@
-import TopInfoBar from "@/components/layout/TopInfoBar";
-import Header from "@/components/layout/Header";
-import Footer from "@/components/layout/Footer";
-import FloatingButtons from "@/components/FloatingButtons";
-import HeroCarousel from "@/components/sections/HeroCarousel";
-import WelcomeSection from "@/components/sections/WelcomeSection";
-import WhyChooseUs from "@/components/sections/WhyChooseUs";
-import Statistics from "@/components/sections/Statistics";
-import ServicesSection from "@/components/sections/ServicesSection";
-import DoctorsSection from "@/components/sections/DoctorsSection";
-import GallerySection from "@/components/sections/GallerySection";
-import TestimonialsSection from "@/components/sections/TestimonialsSection";
-import LocationContact from "@/components/sections/LocationContact";
-import CTABanner from "@/components/sections/CTABanner";
+import React from 'react';
+import HeroCarousel from '../components/HeroCarousel';
+import WelcomeSection from '../components/WelcomeSection';
+import ChooseUs from '../components/ChooseUs';
+import Statistics from '../components/Statistics';
+import FeaturedServices from '../components/FeaturedServices';
+import MeetOurDoctors from '../components/MeetOurDoctors';
+import ReviewsPreview from '../components/ReviewsPreview';
+import CTASection from '../components/CTASection';
 
 const Index = () => {
-  return (
-    <div className="min-h-screen flex flex-col">
-      <TopInfoBar />
-      <Header />
-      
-      <main className="flex-1">
-        <HeroCarousel />
-        <WelcomeSection />
-        <WhyChooseUs />
-        <Statistics />
-        <ServicesSection />
-        <DoctorsSection />
-        <GallerySection />
-        <TestimonialsSection />
-        <LocationContact />
-        <CTABanner />
-      </main>
-      
-      <Footer />
-      <FloatingButtons />
-    </div>
-  );
+    return (
+        <div>
+            <HeroCarousel />
+            <WelcomeSection />
+            <ChooseUs />
+            <Statistics />
+            <FeaturedServices />
+            <MeetOurDoctors />
+            <ReviewsPreview />
+            <CTASection />
+        </div>
+    );
 };
 
 export default Index;
